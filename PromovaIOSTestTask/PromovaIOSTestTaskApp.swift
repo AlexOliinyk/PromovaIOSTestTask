1b4bdd4ec5a7380f48d495dba014de4465e956a7
Alex Oliinyk <alex.oliinyk@gmail.com>
2024-07-25 16:42:57 +0200

    Initial Commit

---
+++ PromovaIOSTestTask/PromovaIOSTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromovaIOSTestTaskApp.swift
+//  PromovaIOSTestTask
+//
+//  Created by Oleksandr Oliinyk on 25/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PromovaIOSTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
